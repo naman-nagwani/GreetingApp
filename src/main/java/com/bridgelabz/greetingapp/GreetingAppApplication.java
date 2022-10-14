73df7049dcd9532fc1c2a30211faf4a0f9c0e850
“naman-nagwani” <namannagwani@gmail.com>
2022-10-14 12:20:40 +0530

    UC2 : Added Service layer

--- src/main/java/com/bridgelabz/greetingapp/GreetingAppApplication.java
+++ src/main/java/com/bridgelabz/greetingapp/GreetingAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreetingAppApplication {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to GreetingApp");
         SpringApplication.run(GreetingAppApplication.class, args);
     }
 
